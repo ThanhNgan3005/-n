3d12a2d10be2fe567b163660c8da421c0c6606d0
Thanh Ngan <ltgan2000152@student.ctuet.edu.vn>
2023-12-15 22:46:54 +0700

    sửa lần 1

--- views/menuAdmin.php
+++ views/menuAdmin.php
@@ -2,7 +2,7 @@
         <a href="admin.php?controller=dashboard" class=" d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom justify-content-center">
         <!-- <span class="fs-5 fw-semibold text-center">Trang Admin</span> -->
         <span class="text-center">
-          <img src="./uploads/img/avarta/quochg.jpg" alt="Logo" width="150" height="150" class="rounded-circle" alt="Cinque Terre"> 
+          <img src="./uploads/img/avarta/avt.jpg" alt="Logo" width="180" height="180" class="rounded-circle" alt="Cinque Terre"> 
           <p class=" mt-1"><?php echo $_SESSION['tenNguoiDung'] ?></p>
         </span>
 
@@ -42,8 +42,8 @@
           </button>
           <div class="collapse" id="dm-collapse">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-4">
-              <li><a href="admin.php?controller=category" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Tất cả danh mục</a></li>
-              <li><a href="admin.php?controller=category&action=insert" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Thêm danh mục</a></li>
+              <li><a href="admin.php?controller=category" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill"></i>Tất cả danh mục</a></li>
+              <li><a href="admin.php?controller=category&action=insert" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Thêm danh mục</a></li>
             </ul>
           </div>
         </li>
@@ -62,9 +62,9 @@
           </button>
           <div class="collapse" id="sp-collapse">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-4">
-                <li><a href="admin.php?controller=product" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Tất cả sản phẩm</a></li>
-                <li><a href="admin.php?controller=product&action=insert" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Thêm sản phẩm</a></li>
-                <li><a href="admin.php?controller=product&action=proDeleted" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Sản phẩm đã xóa</a></li>
+                <li><a href="admin.php?controller=product" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Tất cả sản phẩm</a></li>
+                <li><a href="admin.php?controller=product&action=insert" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Thêm sản phẩm</a></li>
+                <li><a href="admin.php?controller=product&action=proDeleted" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Sản phẩm đã xóa</a></li>
             </ul>
           </div>
         </li>
@@ -85,8 +85,8 @@
           </button>
           <div class="collapse" id="slide-collapse">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-4">
-                <li><a href="admin.php?controller=slide" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Tất cả slide</a></li>
-                <li><a href="admin.php?controller=slide&action=insert" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Thêm slide</a></li>
+                <li><a href="admin.php?controller=slide" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Tất cả slide</a></li>
+                <li><a href="admin.php?controller=slide&action=insert" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Thêm slide</a></li>
             </ul>
           </div>
         </li>
@@ -105,7 +105,7 @@
           </button>
           <div class="collapse" id="hd-collapse">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-4">
-                <li><a href="admin.php?controller=order" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Tất cả hóa đơn</a></li>
+                <li><a href="admin.php?controller=order" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Tất cả hóa đơn</a></li>
             </ul>
           </div>
         </li>
@@ -127,9 +127,9 @@
           </button>
           <div class="collapse" id="account-collapse">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-4">
-                <li><a href="admin.php?controller=user&action=changeInfo" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Thay đổi thông tin</a></li>
-                <li><a href="admin.php?controller=user&action=changePass" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Đổi mật khẩu</a></li>
-                <li><a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-arrow-right-short me-1"></i>Đăng xuất</a></li>
+                <li><a href="admin.php?controller=user&action=changeInfo" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Thay đổi thông tin</a></li>
+                <li><a href="admin.php?controller=user&action=changePass" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Đổi mật khẩu</a></li>
+                <li><a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" class="link-dark d-inline-flex text-decoration-none rounded p-2"><i class="bi bi-caret-right-fill me-1"></i>Đăng xuất</a></li>
 
             </ul>
           </div>
